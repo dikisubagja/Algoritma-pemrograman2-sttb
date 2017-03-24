29b5fe17a62f282b7520e00b23e932348dd0aa3d
dikisubagja <dikisubagja50@gmail.com>
2017-03-24 23:54:40 +0700

    Add files via upload

---
+++ Program_parameter2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+//prototype
+int tambahkan (int a, int b);
+
+
+//Program Utama
+int main () {
+	int a, b ;
+	cout << "Masukan nilai A : " ;
+	cin >> a ;
+	cout << "Masukan nilai B : ";
+	cin >> b ;
+	cout << "Hasil : " <<tambahkan (a, b) << endl ;
+	return 0 ;
+}
+
+int tambahkan (int a, int b) {
+	return a + b ;
+}
